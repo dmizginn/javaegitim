d633f0b282d5ada12abd64c700c99d51552c36e9
dmizginn <80034008+dmizginn@users.noreply.github.com>
2023-04-04 11:45:19 +0300

    Add files via upload

---
+++ metot_palindrom_sayi.java
@@ -0,0 +1,24 @@
+package giris;
+
+public class metot_palindrom_sayi {
+    static boolean isPalindrom(int number){
+    int temp = number, reverseNumber=0, lastNumber;
+    while(temp != 0){
+        //System.out.println("=========");
+        //System.out.println("Sayı: "+temp);
+        lastNumber = temp %10;
+        //System.out.println("Son Basamak: "+lastNumber);
+        reverseNumber=(reverseNumber * 10)+lastNumber;
+        //System.out.println("Yeni Sayı: "+reverseNumber);
+        temp /= 10;
+    }
+    if(number == reverseNumber){
+        return true;
+    }else{
+        return false;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(isPalindrom(528));
+    }
+}
