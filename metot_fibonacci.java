cf9d2b36ff30bec1889d19aad27754ee075ec27c
dmizginn <80034008+dmizginn@users.noreply.github.com>
2023-04-05 09:04:36 +0300

    Add files via upload

---
+++ metot_fibonacci.java
@@ -0,0 +1,27 @@
+package giris;
+
+import java.util.Scanner;
+
+public class metot_fibonacci {
+    static void isFibonacci(int elemanSayisi){
+        int oncekiSayi = 0, simdikiSayi = 1;
+        System.out.print(oncekiSayi + " " + simdikiSayi + " ");
+
+        for (int i = 3; i <= elemanSayisi; i++) {
+            int yeniSayi = oncekiSayi + simdikiSayi;
+            System.out.print(yeniSayi + " ");
+            oncekiSayi = simdikiSayi;
+            simdikiSayi = yeniSayi;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Fibonacci serisi eleman sayısını girin: ");
+        int elemanSayisi = scanner.nextInt();
+
+        isFibonacci(elemanSayisi);
+    }
+
+}
